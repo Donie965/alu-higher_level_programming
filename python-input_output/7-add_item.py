6082de125c504a41c4d355542a1d32d854942248
Donie965 <mdoniegloria@alustudent.com>
2024-11-15 11:57:54 -0800

    S

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Are you docuemnted ?"""
-
-
 import sys
+from 6-load_from_json_file import load_from_json_file
+from 5-save_to_json_file import save_to_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file')\ 
-        .load_from_json_file
+# Define the filename
+filename = "add_item.json"
 
-    try:
-        loadFile = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        loadFile = []
+# Try to load the existing list from the file
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
 
-    argc = len(sys.argv)
-    for idx in range(1, argc):
-        loadFile.append(sys.argv[idx])
-    save_to_json_file(loadFile, "add_item.json")
+# Add command-line arguments to the list (ignoring the script name)
+items.extend(sys.argv[1:])
+
+# Save the updated list back to the file
+save_to_json_file(items, filename)
