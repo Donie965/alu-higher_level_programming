14849dc5f682634c94b46f61e8b4f282538cec91
Donie965 <d.mbonimpa@alustudent.com>
2024-11-27 09:05:31 +0000

    c

--- 0-body_size.sh
+++ 0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script takes in a URL and displays the size of the response body in bytes.
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Use curl to fetch the response body and get the size in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+# Script to display the size of the response body for a given URL
+curl -s "$1" | wc -c
