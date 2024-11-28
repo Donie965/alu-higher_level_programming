8af6a0debc3f2a757ebf55aefedaafe2b34c97ab
Donie965 <mdoniegloria@gmail.com>
2024-11-28 22:44:53 +0200

    do

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,16 +10,26 @@ from sys import argv
 from model_state import Base, State
 from model_city import City
 
-
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
+    # Connect to the MySQL database using the provided arguments
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    
+    # Create tables if they do not exist (creates both State and City tables)
     Base.metadata.create_all(eng)
+    
+    # Create a session to interact with the database
     Session = sessionmaker(bind=eng)
     session = Session()
-    rows = session.query(City, State).filter(City.state_id == State.id)\
-                                     .order_by(City.id).all()
-    for city, state in rows:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    # Query to get all City objects and their corresponding State objects
+    rows = session.query(City, State).filter(City.state_id == State.id).order_by(City.id).all()
+    
+    # If there are records, print them in the desired format
+    if rows:
+        for city, state in rows:
+            print("{}: ({}) {}".format(state.name, city.id, city.name))
+    else:
+        print("No cities found.")
+
+    # Close the session
     session.close()
